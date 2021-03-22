e62a8eb307a6a08a8d87ad0ba4cf7726221fd4af
mesiarm <mesiarm@gmail.com>
2021-03-22 22:35:23 +0100

    homepage
    - laravel mix
    - bootstrap, bootstrap icons
    - layout, homepage
    - gitignore

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="sk">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" type="text/css">
+    <title>Efective cleaning and gardening @yield('title')</title>
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
